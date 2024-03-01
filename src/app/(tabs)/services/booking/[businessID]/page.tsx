7b6aee792b99e88de80192ff7674c48335bc159e
Dany Briceno <dany.briceno65@gmail.com>
2024-03-01 16:42:06 -0600

    fixed timepicker prop

--- src/app/(tabs)/services/booking/[businessID]/page.tsx
+++ src/app/(tabs)/services/booking/[businessID]/page.tsx
@@ -1,31 +1,39 @@
-"use client"
-import { useSearchParams } from "next/navigation"
+"use client";
+import { useSearchParams } from "next/navigation";
 import Header from "@/app/compoents/header";
 import fakeData from "@/app/compoents/fakedata";
 import Calendar from "@/app/compoents/calander";
 import TimePicker from "@/app/compoents/timePicker";
 
-export default function BusinessBooking({}){
-        const searchParams = useSearchParams()
-        const id = searchParams.get('businessID')
-        console.log(id)
-        const selectedBusiness = fakeData.find(business => business.id.toString() === id);
+export default function BusinessBooking({}) {
+  const searchParams = useSearchParams();
+  const id = searchParams.get("businessID");
+  console.log(id);
+  const selectedBusiness = fakeData.find(
+    (business) => business.id.toString() === id
+  );
 
-    return (
-              <>
-                <Header />
-                <Calendar/>
-                <TimePicker/>
-                {selectedBusiness ? (
-                  <div className="p-4 bg-cream"> {/* Ajout d'un padding et d'un fond crème pour plus de lisibilité */}
-                    <h1 className="text-black text-xl font-bold">Booking for Business ID: {selectedBusiness.id}</h1>
-                    <h2 className="text-black text-lg">Title: {selectedBusiness.title}</h2>
-                    {/* Additional content and logic for booking a specific business */}
-                  </div>
-                  
-                ) : (
-                  <h1 className="text-black">Business not found</h1> 
-                )}
-              </>
-            );
+  return (
+    <>
+      <Header />
+      <Calendar />
+      <TimePicker onSelectTime={(time) => console.log(time)} />
+
+      {selectedBusiness ? (
+        <div className="p-4 bg-cream">
+          {" "}
+          {/* Ajout d'un padding et d'un fond crème pour plus de lisibilité */}
+          <h1 className="text-black text-xl font-bold">
+            Booking for Business ID: {selectedBusiness.id}
+          </h1>
+          <h2 className="text-black text-lg">
+            Title: {selectedBusiness.title}
+          </h2>
+          {/* Additional content and logic for booking a specific business */}
+        </div>
+      ) : (
+        <h1 className="text-black">Business not found</h1>
+      )}
+    </>
+  );
 }
